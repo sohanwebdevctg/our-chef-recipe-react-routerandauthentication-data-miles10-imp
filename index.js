81b0bf8a502e98eec9269afb3de31ed0157593bd
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-10-09 22:28:24 +0600

    adding chef.json file and link this file

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const chef = require('./data/chef.json')
 const port = 5000;
 
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send("hello world")
+})
+
+app.get('/chef', (req, res) => {
+  res.send(chef)
 })
 
 app.listen(port, () => {
